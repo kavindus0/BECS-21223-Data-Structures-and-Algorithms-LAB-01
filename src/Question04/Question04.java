13d71fab003ee1bf34de032dc28a86f02bece7a4
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-03-29 21:08:38 +0530

    refactor(Question04): enhance vowel checker logic to sort and count vowels
    Improve the VowelChecker method to sort vowels in ascending order and count occurrences. This change ensures that vowels are displayed in alphabetical order and avoids duplicate entries in the output.

--- src/Question04/Question04.java
+++ src/Question04/Question04.java
@@ -1,25 +1,68 @@
+
 package Question04;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 // Write a program that reads a string
 //and prints the vowels i
 // in ascending order of the alphabet using two stacks.Example:Input:DIALOGUE Output:A E I O U
 public class Question04 {
+    int aCount=0;
+    int eCount=0;
+    int iCount=0;
+    int oCount=0;
+    int uCount=0;
 
+    
+ char[] vowelList = {'a','e','i','o','u'};
 
 public void VowelChecker(String inpString){
 
-    Stack stack = new Stack(inpString.length());
+    Stack stackForWord = new Stack(inpString.length());
+    Stack stackForVowel = new Stack(inpString.length());
 
     char[] inpStringArray = inpString.toCharArray();
    for (char c : inpStringArray) {
-    stack.push(c);
+    stackForWord.push(c);
    }
 
-   System.out.println(stack);
+ArrayList<Character> charArrayList = new ArrayList<>();
+
+
+   
+    for (char c : inpStringArray) {
+        if (c=='a' && aCount == 0) {
+            charArrayList.add(c);
+            aCount++;
+            } else if(c=='e'&& eCount==0){
+                charArrayList.add(c);
+                eCount++;
+            } else if(c=='i'&& iCount==0){
+                charArrayList.add(c);
+                iCount++;
+            } else if(c=='o'&& oCount==0){
+                charArrayList.add(c);
+                oCount++;
+            } else if(c=='u'&& uCount==0){
+                charArrayList.add(c);
+                uCount++;
+            }       
+        }
+    
+
+ 
+    java.util.Collections.sort(charArrayList);
+    
+    Stack outputStack = new Stack(charArrayList.size());
+    for (char c : charArrayList) {
+        outputStack.push(c);
+    }
+    System.out.println(outputStack);
+
+
+    
 
 
 }
-
-
-
 }
--- src/Question04/Test.java
+++ src/Question04/Test.java
@@ -3,7 +3,7 @@ package Question04;
 public class Test {
     public static void main(String[] args) {
         Question04 q4 = new Question04();
-        q4.VowelChecker("hievery");
+        q4.VowelChecker("wooowmane");
     }
     
 }
